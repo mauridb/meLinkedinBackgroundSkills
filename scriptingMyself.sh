8619c3ffe60518c487071cef656b1c0ce4c8ca1e
ColmeSH <mauriziocontatto@gmail.com>
2017-05-20 12:43:45 +0200

    update

---
+++ scriptingMyself.sh
@@ -0,0 +1,36 @@
+admin="mauridb"
+question="$admin, what are your computer programming skills?"
+questionframework="Which frameworks do you know?"
+
+
+mauridbskills=()
+frameworks=()
+echo $question
+
+COUNTER=0
+while :
+do
+  read skill
+  if [ $skill = "next" ]
+  then 
+    break
+  fi
+  mauridbskills[$COUNTER]=$skill
+  let COUNTER=COUNTER+1
+done
+
+echo $questionframework
+INDEX=0
+while :
+do
+    read fw
+    if [ $fw = "end" ]
+    then 
+      break
+    fi
+    frameworks[$INDEX]=$fw 
+    let INDEX=INDEX+1
+done
+echo "SKILLS: ${mauridbskills[*]}"
+echo "FRAMEWORKS:${frameworks[*]}" 
+
